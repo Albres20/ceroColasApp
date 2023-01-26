c0dc71c569c505a917450c4614ba1e30a3c4e13f
David Pablo <david_albres20@hotmail.com>
2023-01-26 15:46:40 -0500

    Interfaz loguin + logica validación

---
+++ app/src/main/java/com/example/cerocolas/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.cerocolas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsuario, edContraseña;
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUsuario=findViewById(R.id.editTextUsuario);
+        edContraseña=findViewById(R.id.editTextContraseña);
+        btn=findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario=edUsuario.getText().toString();
+                String contraseña=edContraseña.getText().toString();
+                boolean pass=validarUsuario(usuario, contraseña);
+                if(pass){
+                    System.out.println("Ingresó...");
+
+                }
+                else{
+                    System.out.println("no ingresó...");
+                }
+            }
+
+        });
+
+    }
+    public boolean validarUsuario(String username, String password) {
+        if (username.isEmpty() || password.isEmpty()) {
+            System.out.println("Error: nombre de usuario o contraseña vacíos.");
+            Toast.makeText(getApplicationContext(), "Usuario o contraseña vacía.", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+        if (username.length() < 8 || password.length() < 8) {
+            System.out.println("Error: nombre de usuario o contraseña debe tener al menos 8 caracteres.");
+            Toast.makeText(getApplicationContext(), "Usuario o contraseña debe tener al menos 8 caracteres.", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+        return true;
+    }
+}
