2091068749c7e3b1f3e015a5a625360e5cfebad2
David Pablo <david_albres20@hotmail.com>
2023-01-31 10:01:14 -0500

    Interfaz registrarse

--- app/src/main/java/com/example/cerocolas/RegisterActivity.java
+++ app/src/main/java/com/example/cerocolas/RegisterActivity.java
@@ -3,8 +3,15 @@ package com.example.cerocolas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername, edEmail, edPassword, edConfirm;
+    Button btn;
+    TextView TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
