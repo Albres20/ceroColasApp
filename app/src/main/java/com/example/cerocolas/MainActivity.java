5f52fa7f60860a282dac448a5113ae23edf5df36
David Pablo <david_albres20@hotmail.com>
2023-02-02 16:13:19 -0500

    presentacion 01 -avance hasta interfaz principal

--- app/src/main/java/com/example/cerocolas/MainActivity.java
+++ app/src/main/java/com/example/cerocolas/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cerocolas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username=sharedPreferences.getString("username")
     }
 }
